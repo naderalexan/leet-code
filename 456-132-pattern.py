675f5c706efcb129942ccc6e9f3fa992d0ced8fd
Nader Alexan <alexan.nader@gmail.com>
2020-07-18 23:10:29 +0200

    feat: 132 pattern

---
+++ 456-132-pattern.py
@@ -0,0 +1,11 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        s = []
+        last = None
+        for cur in nums[::-1]:
+            if last and last > cur:
+                return True
+            while s and s[-1] < cur:
+                last = s.pop()
+            s.append(cur)
+        return False
