f1ba7255e6f29526636e48b3797d35ebbbcd78e2
Nader Alexan <alexan.nader@gmail.com>
2020-07-20 15:18:22 +0200

    chore: styling

--- 130-surrounded-regions.py
+++ 130-surrounded-regions.py
@@ -9,17 +9,17 @@ class Solution:
                 return
 
             b[i][j] = "#"
-            flip_adjacent(b, i+1, j)
-            flip_adjacent(b, i-1, j)
-            flip_adjacent(b, i, j+1)
-            flip_adjacent(b, i, j-1)
+            flip_adjacent(b, i + 1, j)
+            flip_adjacent(b, i - 1, j)
+            flip_adjacent(b, i, j + 1)
+            flip_adjacent(b, i, j - 1)
 
         height = len(board)
         for i in range(height):
             width = len(board[i])
             for j in range(width):
                 # check if not edge cell
-                if not (i==0 or j==0 or i==height-1 or j==width-1):
+                if not (i == 0 or j == 0 or i == height - 1 or j == width - 1):
                     continue
                 if board[i][j] == "O":
                     flip_adjacent(board, i, j)
