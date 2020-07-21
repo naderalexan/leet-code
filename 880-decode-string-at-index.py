35de528b3bdd490c8cb0b821342f005f6650eaf9
Nader Alexan <alexan.nader@gmail.com>
2020-07-21 23:07:37 +0200

    feat: 880

---
+++ 880-decode-string-at-index.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decodeAtIndex(self, S: str, K: int) -> str:
+        n = 0
+        for i, c in enumerate(S):
+            if c.isdigit():
+                n *= int(c)
+            else:
+                n += 1
+            if n >= K: break
+        
+        for j in range(i, -1, -1):
+            c = S[j]
+            if c.isdigit():
+                n /= int(c)
+                K %= n
+                
+            else:
+                if K == 0 or K == n: return c
+                
+                n -= 1
+        
