f7bd4d8fdbce498541df40743c0c13d0c3d16f5c
Nader Alexan <alexan.nader@gmail.com>
2023-09-09 16:05:26 +0300

    feat: 36

---
+++ 36-valid-sudoku.py
@@ -0,0 +1,64 @@
+from typing import List
+from collections import defaultdict
+
+test_case = [
+    (
+        [
+            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+        ],
+        True,
+    ),
+    (
+        [
+            ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+        ],
+        False,
+    ),
+]
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
+
+        for row in range(9):
+            for col in range(9):
+                cur = board[row][col]
+                if cur == ".":
+                    continue
+                if (
+                    cur in rows[row]
+                    or cur in cols[col]
+                    or cur in boxes[(row // 3, col // 3)]
+                ):
+                    return False
+                rows[row].add(cur)
+                cols[col].add(cur)
+                boxes[(row // 3, col // 3)].add(cur)
+        return True
+
+
+for inp, out in test_case:
+    try:
+        actual = Solution().isValidSudoku(inp)
+        assert actual == out
+    except AssertionError:
+        print(f"Inp: {inp}, expected {out}, got {actual}")
