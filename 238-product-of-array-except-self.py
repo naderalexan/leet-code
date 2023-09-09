f62a65729ed7af7b82138da9b57ff58e395a3318
Nader Alexan <alexan.nader@gmail.com>
2023-09-09 15:56:07 +0300

    feat: 238

---
+++ 238-product-of-array-except-self.py
@@ -0,0 +1,23 @@
+from typing import List
+
+test_case = [([1, 2, 3, 4], [24, 12, 8, 6]), ([-1, 1, 0, -3, 3], [0, 0, 9, 0, 0])]
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [1] * len(nums)
+        for i in range(1, len(nums)):
+            result[i] = result[i - 1] * nums[i - 1]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
+        return result
+
+
+for inp, out in test_case:
+    try:
+        actual = Solution().productExceptSelf(inp)
+        assert actual == out
+    except AssertionError:
+        print(f"Inp: {inp}, expected {out}, got {actual}")
